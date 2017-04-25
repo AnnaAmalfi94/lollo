46c832ea56eb049f9c292305ef2a5af4e0666f7c
AnnaAmalfi94 <annamalfi@gmail.com>
2017-04-25 14:49:36 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'prova',
+		wp_ws_url : 'http://wpappkit.altervista.org/wp-appkit-api/prova',
+		wp_url : 'http://wpappkit.altervista.org',
+		theme : 'newsweb',
+		version : '0.1',
+		app_title : 'Newsweb',
+		app_platform : 'android',
+		gmt_offset : 2,
+		debug_mode : 'off',
+		auth_key : 'Z%CG7asYhVjQ@kFajD8&axqoaygIaUkH9cbzyzfOOdAytSG38)K9uC3A0Tm8g1g&',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
